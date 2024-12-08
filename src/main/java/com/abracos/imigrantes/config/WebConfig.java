d1a50f0ee47243b2a9b4d4ff411ba57c421a36b2
iarley <iarleysouza615@gmail.com>
2024-12-08 02:23:53 -0300

    update

--- src/main/java/com/abracos/imigrantes/config/WebConfig.java
+++ src/main/java/com/abracos/imigrantes/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                        "https://backend.abracos.tech"
+                        "https://demoday-abracos.github.io"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
